c83d83f74f248d1213b1bb3099251c10c1027274
zvestigo <zvestigo@icloud.com>
2020-12-15 03:01:44 -0500

    Styled layout, added options toggle

--- js/main.js
+++ js/main.js
@@ -1,17 +1,14 @@
-console.log("test");
+/* Declared variables for html elements */
+var intro = document.getElementById("intro");
+var charDesc = document.getElementById("char-desc");
+var comment = document.getElementById("comment");
+var rollBtn = document.getElementById("roll-btn");
+var d20 = document.getElementById("d20");
 
-/* Object containing nested arrays from which to generate character */
-var charOptions = {
-    race: ["Dwarf", "Elf", "Halfling", "Human", "Dragonborn", "Gnome", "Half-Elf", "Half-Orc", "Tiefling"],
-    class: ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"],
-    background: ["Acolyte", "Charlatan", "Criminal", "Entertainer", "Folk Hero", "Guild Artisan","Hermit", "Noble", "Outlander", "Sage", "Sailor", "Urchin"],
-    adjective: ["A Grumpy", "A Cheerful", "A Colorblind", "A Paranoid", "An Absent-minded", "An Abrasive", "A Dishonest", "An Easygoing", "A Near-sighted", "A Quiet", "A Boistrous", "An Illiterate", "A Shady", "A Caring","A Quirky"],
-    quirk: ["hates the color yellow", "is deathly allergic to bees", "believes everyone is good at heart", "would sell their mother for 20 gold pieces", "likes to pull pranks on their party", "has a pet ferret", "rides a miniature horse", "lives with their mother", ]
-}
 
 /* Functioon to retrieve a entry in charOptions*/
 function rollTrait(trait) {
-    var i = Math.floor(Math.random() * trait.length);
+    var i = Math.floor(Math.random() * trait.length);  // random number corresponding to # of options for given trait
     var randomTrait = trait[i];
     return randomTrait;
 }
@@ -27,15 +24,23 @@ function createCharacter() {
         quirk: rollTrait(charOptions.quirk)
     };
 
-    console.log(character.adjective + " " + character.race + " " + character.class + " who grew up as a " + character.background + " and " + character.quirk)
+    console.log(character.adjective + " " + character.race + " " + character.class + " who grew up as a " + character.background + " and " + character.quirk) // left to later make it possible to create a log for user to check
 
-    var charDesc = document.getElementById("char-desc");
     charDesc.innerText = character.adjective + " " + character.race + " " + character.class + " who grew up as a " + character.background + " and " + character.quirk;
+    
+    intro.style.opacity = "1";       // Fading text in on first roll
     charDesc.style.opacity = "1";
-
-    return character;
+    comment.style.opacity = "1";
 }
 
-var rollBtn = document.getElementById("roll-btn");
-
 rollBtn.addEventListener('click', createCharacter);
+
+
+/* Object containing nested arrays from which to generate character */
+var charOptions = {
+    race: ["Dwarf", "Elf", "Halfling", "Human", "Dragonborn", "Gnome", "Half-Elf", "Half-Orc", "Tiefling"],
+    class: ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"],
+    background: ["acolyte", "charlatan", "criminal", "entertainer", "folk Hero", "guild artisan","hermit", "noble", "outlander", "sage", "sailor", "urchin"],
+    adjective: ["A grumpy", "A cheerful", "A colorblind", "A paranoid", "An absent-minded", "An abrasive", "A dishonest", "An easygoing", "A near-sighted", "A quiet", "A boistrous", "An illiterate", "A shady", "A caring","A quirky"],
+    quirk: ["hates the color yellow", "is deathly allergic to bees", "believes everyone is good at heart", "would sell their mother for 20 gold pieces", "likes to pull pranks on their party", "has a pet ferret", "rides a miniature horse", "lives with their mother", ]
+}
